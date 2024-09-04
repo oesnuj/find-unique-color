4af53fe1eb8c9a159682405156dc4c193df43d55
Junseo Kim <semiwest2@gmail.com>
2024-09-04 13:04:03 +0900

    getRank API 파라미터 추가

--- src/api/getRank.js
+++ src/api/getRank.js
@@ -1,9 +1,17 @@
 import axios from "axios";
 
-export const getRank = () => {
-
-    return axios.get('https://wwwag-backend.co.kr/ranking/list').then((response) => {
-        const rankData = response.data;
-        return rankData;
-    })
-}
+export const getRank = async (pageNumber = 0, pageSize = 10) => {
+    try {
+        const response = await axios.get('https://wwwag-backend.co.kr/ranking/list', {
+            params: {
+                pageNumber: pageNumber,
+                pageSize: pageSize
+            }
+        });
+        console.log('Rank data:', response.data);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching rank data:', error.response ? error.response.data : error.message);
+        throw error;
+    }
+};
