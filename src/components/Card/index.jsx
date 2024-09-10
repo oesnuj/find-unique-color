10857713e3f5ddbd327f695b91495d9234905c1c
Junseo Kim <semiwest2@gmail.com>
2024-09-10 17:46:40 +0900

    불필요 console.log 제거

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import * as Styled from './styled';
 
 function Card({ size, color, onClick, flash }) {
-  console.log('Card Flash Prop:', flash); // 디버깅
   return <Styled.Card onClick={onClick} color={color} size={size} flash={flash} />;
 }
 
