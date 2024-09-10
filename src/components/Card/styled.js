1f2766a14109b27cbfac4a0ce251a32011b8cd1a
Junseo Kim <semiwest2@gmail.com>
2024-09-10 17:36:32 +0900

    정답 카드 애니메이션 keyframes 추가

--- src/components/Card/styled.js
+++ src/components/Card/styled.js
@@ -1,4 +1,13 @@
-import styled from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
+
+const flashAnimation = keyframes`
+  0%, 100% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0;
+  }
+`;
 
 export const Card = styled.div`
   display: flex;
@@ -6,11 +15,14 @@ export const Card = styled.div`
   align-items: center;
   color: white;
   background-color: ${({ color }) => `${color}`};
-
   width: ${({ size }) => `${size}px`};
   height: ${({ size }) => `${size}px`};
   box-sizing: border-box;
   border: 0.5px white solid;
+
+  ${({ flash }) => flash && css`
+    animation: ${flashAnimation} 2s infinite;
+  `}
 
   :hover {
     cursor: pointer;
