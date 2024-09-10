a26ab78e686795fe7c27cef74efd2dae02cd22d4
Junseo Kim <semiwest2@gmail.com>
2024-09-10 17:36:21 +0900

    정답 카드 애니메이션 props 추가

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import * as Styled from './styled';
 
-function Card({ size, color, onClick }) {
-  return <Styled.Card onClick={onClick} color={color} size={size} />;
+function Card({ size, color, onClick, flash }) {
+  console.log('Card Flash Prop:', flash); // 디버깅
+  return <Styled.Card onClick={onClick} color={color} size={size} flash={flash} />;
 }
 
 export default Card;
