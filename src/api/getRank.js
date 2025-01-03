66abc575f3ea1c66ed15d3f5121de3e338e98b1e
JangHongJoon <wkdghdwns19969@gmail.com>
2025-01-03 21:02:30 +0900

    fix : 백엔드 서버 변경으로 인해 API 호출 코드 변경

--- src/api/getRank.js
+++ src/api/getRank.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getRank = async (pageNumber = 0, pageSize = 10) => {
     try {
-        const response = await axios.get('https://wwwag-backend.co.kr/ranking/list', {
+        const response = await axios.get('https://wwwag-backend.co.kr:5000/ranking/list', {
             params: {
                 pageNumber: pageNumber,
                 pageSize: pageSize
--- src/api/postRank.js
+++ src/api/postRank.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export function postRank(userName, stage, point) {
   console.log(point)
-  axios.post('https://wwwag-backend.co.kr/ranking', {
+  axios.post('https://wwwag-backend.co.kr:5000/ranking', {
     nickname: userName,
     stage : stage,
     score : point,
